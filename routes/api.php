e17cfb8d27559d4536fa5e05818fad6d05f1d2a4
Thierry Matteucci <thierry.matteucci@gmail.com>
2025-12-27 15:48:04 -0500

    Création des routes

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,18 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 
-
-//Routes du TP2 ici : 
+//Routes du TP2 ici :
 Route::get('/films', 'App\Http\Controllers\FilmController@index');
+
+// J'applique ici le throttling à toutes les routes (5 tentatives par minute)
+Route::middleware('throttle:5,1')->group(function () {
+
+    Route::post('/signup', 'App\Http\Controllers\AuthController@signup');
+    Route::post('/signin', 'App\Http\Controllers\AuthController@signin');
+
+    // J'ai créé un sous-groupe de middleware au cas ou on aurait besoin d'en ajouter d'autres plus tard
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/signout', 'App\Http\Controllers\AuthController@signout');
+    });
+});
+
